eec0960f9b26d82cef0d191c891472d0ac10ba25
bdobak <balint.dobak@gmail.com>
2026-03-07 19:58:56 +0100

    Update gaussarea.py

--- gaussarea.py
+++ gaussarea.py
@@ -4,7 +4,7 @@ from scipy.integrate import quad
 
 A=float(input("Enter A: "))
 x0=float(input("Enter x0: "))
-Asig=float(input("Enter sigma: "))
+sig=float(input("Enter sigma: "))
 z0=float(input("Enter z0= "))
 a=float(input("Enter the lower limit: "))
 b=float(input("Enter upper limit: "))
@@ -21,5 +21,6 @@ print(f"The area under the curve from {a} to {b} is {A1[0]}")
 x1=np.linspace(a,b,200)
 y1=gauss(x1,A,x0,sig,z0)
 plt.fill_between(x1,y1)
+
 
 plt.show()
